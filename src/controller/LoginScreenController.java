e036d393a9a5c050bbc1bf6688b58c47c72a75d3
MagiShi <maggieqshi@gmail.com>
2016-09-20 22:05:10 -0400

    continuing login screen controller

--- src/controller/LoginScreenController.java
+++ src/controller/LoginScreenController.java
@@ -1,9 +1,37 @@
+package src.controller;
+
 /**
  * Created by Dain on 9/20/2016.
  */
-
+import src.fxapp.WaterzMainFXApplication;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 public class LoginScreenController {
 
+    /** a link back to the main application class */
+    private WaterzMainFXApplication mainApplication;
 
+    /* references to the widgets in the fxml file */
+
+
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the constructor and
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+    }
+
+
+
+    /**
+     * Setup the main application link so we can call methods there
+     *
+     * @param mainFXApplication  a reference (link) to our main class
+     */
+    public void setMainApp(WaterzMainFXApplication mainFXApplication) {
+    }
 }
+
