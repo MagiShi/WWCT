3a5dfb0f0094a9181baf5fc1acebfebe5b335e8a
dainryoo <dainryoo@gmail.com>
2016-09-20 22:01:27 -0400

    Finished WaterzMainFXApplication

--- src/fxapp/WaterzMainFXApplication.java
+++ src/fxapp/WaterzMainFXApplication.java
@@ -3,8 +3,11 @@ package src.fxapp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import src.controller.MainScreenController;
+import src.controller.LoginScreenController;
 
 import java.io.IOException;
 
@@ -18,6 +21,7 @@ public class WaterzMainFXApplication extends Application {
     public void start(Stage primaryStage) {
         mainScreen = primaryStage;
         initRootLayout(mainScreen);
+        showLogin(mainScreen);
     }
 
     public Stage getMainScreen() {
@@ -45,9 +49,25 @@ public class WaterzMainFXApplication extends Application {
 
     }
 
+    private void showLogin(Stage mainScreen) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(WaterzMainFXApplication.class.getResource("../view/LoginScreen.fxml"));
+            AnchorPane loginScreen = loader.load();
+
+            rootLayout.setCenter(loginScreen);
+
+            LoginScreenController controller = loader.getController();
+            controller.setMainApp(this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
 
 
-    }
+}
