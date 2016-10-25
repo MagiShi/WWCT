a584a92363c507cd53f0ebefc2dd39977c1edba4
spuri9 <surinapuri21@gmail.com>
2016-10-24 20:17:29 -0400

    worker-features

---
+++ src/controller/ManagerSourceDetailScreenController.java
@@ -0,0 +1,184 @@
+package src.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import src.fxapp.WaterzMainFXApplication;
+import src.model.User;
+import src.model.Location;
+import src.model.WaterSource;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by Dain on 10/16/2016.
+ */
+public class ManagerSourceDetailScreenController {
+
+    private WaterzMainFXApplication mainApplication;
+
+    private AnchorPane anchorLayout;
+
+    private WaterSource currentSource;
+
+    private Location currLoc;
+
+    @FXML
+    private Button backButton;
+    @FXML
+    private ListView<String> reportsList;
+    @FXML
+    private Label sourceLocation;
+    @FXML
+    private Label waterType;
+    @FXML
+    private Label waterCondition;
+    @FXML
+    private Label sourceCreator;
+    @FXML
+    private Label creationDate;
+
+    private User currentUser;
+    public void setUser(User newUser) {
+        currentUser = newUser;
+    }
+
+
+    public void setLocation (Location loc) { currLoc = loc; }
+
+    private ArrayList<WaterSource> allReports = new ArrayList<WaterSource>();
+
+    private ArrayList<WaterSource> thisSource = new ArrayList<WaterSource>();
+
+
+
+
+    @FXML protected void backButtonAction() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/ManagerMapScreen.fxml"));
+
+            anchorLayout = fxmlLoader.load();
+            WorkerMapScreenController msc = fxmlLoader.getController();
+
+            msc.setApp(mainApplication);
+            msc.setState(mainApplication.getMainScreen());
+            msc.setUpMapView(mainApplication.getMainScreen());
+
+            msc.setUser(currentUser);
+            Scene scene2 = new Scene(anchorLayout);
+            mainApplication.getMainScreen().setScene(scene2);
+            msc.setMainApp(mainApplication);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML protected void viewReportListButtonAction() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/ManagerReportsList.fxml"));
+
+            anchorLayout = fxmlLoader.load();
+            ManagerReportsListScreenController controller = fxmlLoader.getController();
+            controller.setUser(currentUser);
+            controller.setLocation(currLoc);
+            controller.setThisSource(thisSource);
+
+            Scene scene2 = new Scene(anchorLayout);
+            mainApplication.getMainScreen().setScene(scene2);
+
+            controller.setMainApp(mainApplication);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @FXML protected void addReportButtonAction() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/waterQualityReportScreen.fxml"));
+
+            anchorLayout = fxmlLoader.load();
+            waterQualityReportScreenController controller = fxmlLoader.getController();
+
+            Scene scene2 = new Scene(anchorLayout);
+            mainApplication.getMainScreen().setScene(scene2);
+
+            controller.setMainApp(mainApplication);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the constructor and
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+        boolean alreadyExists = new File("sourceReports.csv").exists();
+        if (alreadyExists) {
+            try (BufferedReader br = new BufferedReader(new FileReader("sourceReports.csv"))) {
+                String line = "";//make into water source
+                while (((line = br.readLine()) != null)) {
+                    String[] data = line.split(",");
+                    String reportNum = data[0];
+                    String name = data[1];
+                    String dateTime = data[2];
+                    String locationName = data[3];
+                    Double lat = Double.parseDouble(data[4]);
+                    Double longit = Double.parseDouble(data[5]);
+                    String type = data[6];
+                    String sourceCondition = data[7];
+
+                    WaterSource wc = new WaterSource(name, dateTime,locationName, lat, longit, type, sourceCondition);
+
+                    allReports.add(wc);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void setCurrentSource(Location newSource) {
+        currLoc = newSource;
+
+        for (WaterSource source: allReports) {
+            if (source.getLatitude().equals(currLoc.getLatitude()) && source.getLongitude().equals(currLoc.getLongitude())) {
+                currentSource = source;
+                thisSource.add(currentSource);
+            }
+        }
+
+        sourceLocation.setText(currentSource.getLocation());
+        waterType.setText(currentSource.getType());
+        waterCondition.setText((currentSource.getSourceCondition()));
+        sourceCreator.setText((currentSource.getUser()));
+        creationDate.setText(currentSource.getDate());
+    }
+
+
+
+    /**
+     * Setup the main application link so we can call methods there
+     *
+     * @param mainFXApplication  a reference (link) to our main class
+     */
+    public void setMainApp(WaterzMainFXApplication mainFXApplication) {
+        mainApplication = mainFXApplication;
+
+    }
+
+}
---
+++ src/controller/UserMapScreenController.java
@@ -0,0 +1,362 @@
+package src.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import src.fxapp.WaterzMainFXApplication;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import src.model.User;
+import src.model.WaterSource;
+
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.event.UIEventType;
+import com.lynden.gmapsfx.javascript.object.*;
+
+import javafx.event.EventHandler;
+import javafx.fxml.Initializable;
+
+import src.model.Facade;
+import src.model.Location;
+import netscape.javascript.JSObject;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class UserMapScreenController implements Initializable, MapComponentInitializedListener {
+
+    /** a link back to the main application class */
+    private WaterzMainFXApplication mainApplication;
+
+    private AnchorPane anchorLayout;
+
+    private User currentUser;
+    private Facade fac;
+
+    private String username;
+    private ArrayList<WaterSource> water = new ArrayList<WaterSource>();
+
+
+    /* references to the widgets in the fxml file */
+
+    @FXML
+    private Button logoutButton;
+    @FXML
+    private Button profileButton;
+    @FXML
+    private Button submitButton;
+    @FXML
+    private BorderPane mapViewer;
+
+    /** a gui view provided by the GMapFX library */
+    private GoogleMapView mapView;
+    /** the actual javascript interface for the google map itself */
+    private GoogleMap map;
+    /** a reference back to the main application object in case we need something or to transition to another window */
+    private WaterzMainFXApplication theApp;
+    /**remember stage for dialogs */
+    private Stage mainStage;
+
+    public UserMapScreenController(WaterzMainFXApplication app, Stage stage) {
+        theApp = app;
+        mainStage = stage;
+        setUpMapView(stage);
+    }
+
+    public UserMapScreenController() {
+    }
+
+    protected void setApp(WaterzMainFXApplication app) {
+        theApp = app;
+    }
+
+    protected void setState (Stage stage) {
+        mainStage = stage;
+    }
+
+    @FXML protected void handleSubmitButtonAction() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/waterSourceReportScreen.fxml"));
+
+            anchorLayout = fxmlLoader.load();
+            waterSourceReportScreenController controller = fxmlLoader.getController();
+            controller.setUser(currentUser);
+            fac = Facade.getInstance();
+            controller.setFacade(fac);
+            Scene scene2 = new Scene(anchorLayout);
+            mainApplication.getMainScreen().setScene(scene2);
+
+            controller.setMainApp(mainApplication);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @FXML protected void handleLogoutButtonAction() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/WelcomeScreen.fxml"));
+
+            anchorLayout = fxmlLoader.load();
+            WelcomeScreenController controller = fxmlLoader.getController();
+
+            Scene scene2 = new Scene(anchorLayout);
+            mainApplication.getMainScreen().setScene(scene2);
+
+            controller.setMainApp(mainApplication);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML protected void handleProfileButtonAction() {
+        //logout stuff :)
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/UserProfile.fxml"));
+
+            anchorLayout = fxmlLoader.load();
+            ProfileScreenController controller = fxmlLoader.getController();
+            Scene scene2 = new Scene(anchorLayout);
+            controller.setUser(currentUser);
+            mainApplication.getMainScreen().setScene(scene2);
+            controller.setMainApp(mainApplication);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setUser(User newUser) {
+        currentUser = newUser;
+    }
+
+
+    /**
+     * Setup the main application link so we can call methods there
+     *
+     * @param mainFXApplication  a reference (link) to our main class
+     */
+    public void setMainApp(WaterzMainFXApplication mainFXApplication) {
+        mainApplication = mainFXApplication;
+    }
+    /**
+     * Construct the google map, set up the different parts of the layout
+     *
+     * @param stage the stage to put the map scene into
+     */
+    protected void setUpMapView(Stage stage) {
+        //construct the view
+        mapView = new GoogleMapView();
+        //we cannot do anything until the map is constructed, so we need a callback
+        //this is provided by the listener.  this class implements the MapComponentInitializedListener
+        //interface
+        mapView.addMapInializedListener(this);
+        //put the map into the center of the border layout
+        mapViewer.setCenter(mapView);
+        //put the map into the scene
+        Scene scene = new Scene(mapViewer);
+        stage.setScene(scene);
+    }
+    /**
+     * constructs the menubar and all the subitems
+     *
+     * @return the menubar after it is constructed
+     */
+    private Node makeMenuBar() {
+        MenuBar mb = new MenuBar();
+        // --- Menu File
+        Menu menuFile = new Menu("File");
+        addFileOptions(menuFile);
+        // --- Menu Edit
+        Menu menuEdit = new Menu("Edit");
+        addEditOptions(menuEdit);
+        // --- Menu View
+        Menu menuView = new Menu("View");
+        addViewOptions(menuView);
+        mb.getMenus().addAll(menuFile, menuEdit, menuView);
+        return mb;
+    }
+    /**
+     * helper to add all the submenu items for the View menu
+     *
+     * @param menuView  a reference to the top level View menu
+     */
+    private void addViewOptions(Menu menuView) {
+    }
+    /**
+     * helper to add all the submenu items for the Edit menu
+     *
+     * @param menuEdit a reference to the top level Edit menu
+     */
+    private void addEditOptions(Menu menuEdit) {
+    }
+    /**
+     * helper to construct the file menu
+     *
+     * @param menuFile reference to the top level File menu
+     */
+    private void addFileOptions(Menu menuFile) {
+        MenuItem openText = new MenuItem("Open Text");
+        openText.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                FileChooser fc = new FileChooser();
+                fc.setTitle("Open Text File");
+                File file  = fc.showOpenDialog(mainStage);
+                if (file != null)
+                    Facade.getInstance().loadModelFromText(file);
+            }
+        });
+
+        MenuItem openBinary = new MenuItem("Open Binary");
+        openBinary.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                FileChooser fc = new FileChooser();
+                fc.setTitle("Open Binary File");
+                File file  = fc.showOpenDialog(mainStage);
+                if (file != null)
+                    Facade.getInstance().loadModelFromBinary(file);
+            }
+        });
+
+        MenuItem openJson = new MenuItem("Open JSON");
+        openJson.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                FileChooser fc = new FileChooser();
+                fc.setTitle("Open JSON File");
+                File file  = fc.showOpenDialog(mainStage);
+                if (file != null)
+                    Facade.getInstance().loadModelFromJson(file);
+            }
+        });
+
+        MenuItem saveText = new MenuItem("Save Text");
+        saveText.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                FileChooser fc = new FileChooser();
+                fc.setTitle("Save Text File");
+                File file  = fc.showSaveDialog(mainStage);
+                if (file != null)
+                    Facade.getInstance().saveModelToText(file);
+            }
+        });
+
+        MenuItem saveBinary = new MenuItem("Save Binary");
+        saveBinary.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                FileChooser fc = new FileChooser();
+                fc.setTitle("Save Binary File");
+                File file  = fc.showSaveDialog(mainStage);
+                if (file != null)
+                    Facade.getInstance().saveModelToBinary(file);
+            }
+        });
+
+        MenuItem saveJson = new MenuItem("Save JSON");
+        saveJson.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                FileChooser fc = new FileChooser();
+                fc.setTitle("Save JSON File");
+                File file  = fc.showSaveDialog(mainStage);
+                if (file != null)
+                    Facade.getInstance().saveModelToJson(file);
+            }
+        });
+
+        MenuItem close = new MenuItem("Close");
+        close.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
+                theApp.closeMapView();
+            }
+        });
+
+        menuFile.getItems().addAll(openText, openBinary, openJson, new SeparatorMenuItem(),
+                saveText, saveBinary, saveJson, new SeparatorMenuItem(),
+                close);
+    }
+
+    @Override
+    public void mapInitialized() {
+        mapView.addMapInializedListener(this);
+
+        //Set the initial properties of the map
+
+        MapOptions options = new MapOptions();
+
+        //set up the center location for the map
+        LatLong center = new LatLong(34, -88);
+
+        options.center(center)
+                .zoom(9)
+                .overviewMapControl(false)
+                .panControl(false)
+                .rotateControl(false)
+                .scaleControl(false)
+                .streetViewControl(false)
+                .zoomControl(false)
+                .mapType(MapTypeIdEnum.TERRAIN);
+
+        map = mapView.createMap(options);
+
+        /** now we communciate with the model to get all the locations for markers */
+
+        Facade fac = Facade.getInstance();
+        List<Location> locations = fac.getLocations();
+
+        //create pins
+        for (Location l: locations) {
+            MarkerOptions markerOptions = new MarkerOptions();
+            LatLong loc = new LatLong(l.getLatitude(), l.getLongitude());
+            markerOptions.position( loc )
+                    .visible(Boolean.TRUE)
+                    .title(l.getTitle());
+            Marker marker = new Marker( markerOptions );
+            map.addUIEventHandler(marker,
+                    UIEventType.click,
+                    (JSObject obj) -> {
+                        InfoWindowOptions infoWindowOptions = new InfoWindowOptions();
+                        infoWindowOptions.content(l.getDescription() );
+                        InfoWindow window = new InfoWindow(infoWindowOptions);
+                        window.open(map, marker);});
+            map.addMarker(marker);
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        boolean alreadyExists = new File("sourceReports.csv").exists();
+        if (alreadyExists) {
+            try (BufferedReader br = new BufferedReader(new FileReader("sourceReports.csv"))) {
+                String line = "";
+                while (((line = br.readLine()) != null)) {
+                    String[] data = line.split(",");
+                    WaterSource source = new WaterSource(data[1], data[2], data[3], Double.valueOf(data[4]), Double.valueOf(data[5]), data[6], data[7]);
+                    water.add(source);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
