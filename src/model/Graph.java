f1c269a7f78bfbb3f67928b58ca856a034e26833
cmullins77 <mullins.cassie@gmail.com>
2016-11-10 12:18:25 -0500

    Javadocs for Graph

--- src/model/Graph.java
+++ src/model/Graph.java
@@ -22,7 +22,10 @@ public class Graph {
     private final ArrayList<Integer> allYearList;
     private final ArrayList<Integer> currentYearList;
 
-
+    /**
+     * The constructor for a Graph
+     * @param reports An iterable list of reports whose data is to be displayed
+     */
     public Graph(Iterable<Report> reports) {
         allYearList = new ArrayList<>();
         allMonthList = new ArrayList<>();
@@ -47,6 +50,10 @@ public class Graph {
         contaminantDisplayed = false;
     }
 
+    /**
+     * Setting a new year to be displayed on the graph
+     * @param newYear The year desired
+     */
     public void setCurrentYear(int newYear) {
         if ((newYear < 2000) || (newYear > 2020)) {
             throw new IllegalArgumentException("The year is not valid");
@@ -77,6 +84,10 @@ public class Graph {
             }
         }
     }
+
+    /**
+     * Setting the graph to show all years from 2000 - 2020
+     */
     public void setShowAll() {
         allYears = true;
         currentYear = null;
@@ -100,6 +111,11 @@ public class Graph {
             }
         }
     }
+
+    /**
+     * Change whether virus is displayed or not
+     * @param b Boolean value of whether virus is displayed
+     */
     public void setVirusDisplayed(Boolean b) {
         virusDisplayed = b;
         if (allYears) {
@@ -108,6 +124,11 @@ public class Graph {
             setCurrentYear(currentYear);
         }
     }
+
+    /**
+     * Change whether contaminant is displayed or not
+     * @param b Boolean value of whether contaminant is displayed
+     */
     public void setContaminantDisplayed(Boolean b) {
         contaminantDisplayed = b;
         if (allYears) {
@@ -116,45 +137,102 @@ public class Graph {
             setCurrentYear(currentYear);
         }
     }
+
+    /**
+     * Getter for current month list
+     * @return ArrayList of current month data
+     */
     public ArrayList<Integer> getCurrentMonthList() {
         return currentMonthList;
     }
+    /**
+     * Getter for current year list
+     * @return ArrayList of current year data
+     */
     public ArrayList<Integer> getCurrentYearList() {
         return currentYearList;
     }
+    /**
+     * Getter for current virus data list
+     * @return ArrayList of current virus data
+     */
     public ArrayList<Float> getCurrentVirusNumList() {
         return currentVirusNumList;
     }
+    /**
+     * Getter for current contaminant data list
+     * @return ArrayList of current contaminant data
+     */
     public ArrayList<Float> getCurrentContamNumList() {
         return currentContamNumList;
     }
+    /**
+     * Getter for boolean of whether all years are displayed
+     * @return Boolean value, true if all years are displayed, false if one year is displayed
+     */
     public boolean getAllYears() {
         return allYears;
     }
+    /**
+     * Getter for the upper value to be displayed on the x-axis
+     * @return and int that is the upper x-axis value
+     */
     public int getUpperXNum() {
         return upperXNum;
     }
+    /**
+     * Getter for the lower value to be displayed on the x-axis
+     * @return and int that is the lower x-axis value
+     */
     public int getLowerXNum() {
         return lowerXNum;
     }
+    /**
+     * Getter for the label to be displayed on the x-axis
+     * @return a string to be used as the x-axis label
+     */
     public String getxAxisLabel() {
         return xAxisLabel;
     }
+    /**
+     * Getter for boolean of whether virus data is displayed
+     * @return Boolean value, true if virus data is displayed, false if it is not
+     */
     public boolean getVirusDisplayed() {
         return virusDisplayed;
     }
+    /**
+     * Getter for boolean of whether contaminant data is displayed
+     * @return Boolean value, true if contaminant data is displayed, false if it is not
+     */
     public boolean getContaminantDisplayed() {
         return contaminantDisplayed;
     }
+    /**
+     * Getter for all virus data list
+     * @return ArrayList of all virus data
+     */
     public ArrayList<Float> getAllVirusNumList() {
         return allVirusNumList;
     }
+    /**
+     * Getter for all contaminant data list
+     * @return ArrayList of all contaminant data
+     */
     public ArrayList<Float> getAllContamNumList() {
         return allContamNumList;
     }
+    /**
+     * Getter for all month data list
+     * @return ArrayList of all months
+     */
     public ArrayList<Integer> getAllMonthList() {
         return allMonthList;
     }
+    /**
+     * Getter for all year data list
+     * @return ArrayList of all year
+     */
     public ArrayList<Integer> getAllYearList() {
         return allYearList;
     }
