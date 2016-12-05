3fa5b556bc5c8b534e41113f68bd584e47cde58a
cmullins77 <mullins.cassie@gmail.com>
2016-12-05 15:46:07 -0500

    Welcome Screen Done

---
+++ src/JavaScript/welcomeJS.js
@@ -0,0 +1,6 @@
+function loginButtonPress() {
+    window.location.href = "../HTML/loginScreen.html";
+}
+function registerButtonPress() {
+    window.location.href = "../HTML/registerScreen.html";
+}
